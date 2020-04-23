6876a2551e6c060e11257161d4694849301d2ea3
Lucas Lacerda Lima <lucaslacerda@riseup.net>
2020-04-23 18:32:51 -0300

    contact social midia

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,6 +5,13 @@ function contact() {
   return (
       <div class="container">
         Essa é a tela de contatos
+        <div class="redes">
+          <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+          <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+          <a href="#"><i class="fab fa-linkedin fa-2x"></i></a>
+          <a href="#"><i class="fab fa-github fa-2x"></i></a>
+        </div>
       </div>
   );
 }
